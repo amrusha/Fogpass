edf4d224abff61dd65b86d382e8d58997b057c2a
firefly00000 <abhinavjssit1@gmail.com>
2018-04-13 01:58:39 +0530

    added display frame

--- app/src/main/java/com/example/abhinav/work/MainActivity.java
+++ app/src/main/java/com/example/abhinav/work/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.abhinav.work;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Button button1;
 
-    String[] bankNames={"BOI","SBI","HDFC","PNB","OBC"};
+    String[] bankNames={"UP" , "DOWN"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1= (Button) findViewById(R.id.submit);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        DisplayActivity.class);
+                startActivity(intent);
+            }
+        });
 //Getting the instance of Spinner and applying OnItemSelectedListener on it
         Spinner spin = (Spinner) findViewById(R.id.spinner);
         spin.setOnItemSelectedListener(this);
@@ -39,5 +51,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> arg0) {
 // TODO Auto-generated method stub
 
+
     }
+
 }
